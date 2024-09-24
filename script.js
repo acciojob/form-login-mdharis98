3db7ebe2c3b58bb7ffeee3c8dd49b35d3a3ee6cf
Mohammad Haris <112751385+mdharis98@users.noreply.github.com>
2024-09-25 00:18:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function getFormvalue() {
-    //Write your code here
 
-}
+function getFormvalue() {
+            // Access the form element
+            const form = document.forms["form1"];
+
+            // Get the values of fname and lname
+            const fname = form["fname"].value;
+            const lname = form["lname"].value;
+
+            // Display the result in an alert
+            alert(fname + " " + lname);
+        }
